6e19e37851748f759c82dcb6fb81f05572af785e
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2020-11-30 16:44:31 -0500

    Update FLAMING_O_DROP.sol
    nit so tokenId is flexible

--- contracts/FLAMING_O_DROP.sol
+++ contracts/FLAMING_O_DROP.sol
@@ -19,37 +19,34 @@ interface IERC20transferFrom { // interface for erc20 token `transferFrom()`
     function transferFrom(address from, address to, uint256 amount) external returns (bool);
 }
 
-interface IERC721transferFrom { // interface for erc721 token `transferFrom()`
+interface IERC721Listing { // brief interface for erc721 token listing
+    function ownerOf(uint256 tokenId) external view returns (address);
+    function tokenByIndex(uint256 index) external view returns (uint256);
+    function totalSupply() external view returns (uint256);
     function transferFrom(address from, address to, uint256 tokenId) external;
 }
 
-interface IFL_O {
-    function ownerOf(uint256 tokenId) external view returns (address);
-    function totalSupply() external view returns (uint256);
-}
-
 contract FLAMING_O_DROP {
-    address public FLAMING_O = 0xc7886c91fF20dE17c9161666202b8D8953D03BBD;
+    address public FLAMING_O = 0xFa8aff6B27B695e30C751609aDf5e10a7bD9b0db;
     
-    function dropERC20(address token, uint256 amount) external { // drop token amount evenly on FL_O holders
-        IFL_O FL_O = IFL_O(FLAMING_O);
-        uint256 count = 1;
-        uint256 length = FL_O.totalSupply();
-        uint256 split = amount / length;
+    function dropERC20(address token, uint256 amount) external { // drop sum token amount evenly on FL_O holders
+        IERC721Listing fl_o = IERC721Listing(FLAMING_O);
+        uint256 count;
+        uint256 length = fl_o.totalSupply();
         
         for (uint256 i = 0; i < length; i++) {
-            IERC20transferFrom(token).transferFrom(msg.sender, FL_O.ownerOf(count), split);
+            IERC20transferFrom(token).transferFrom(msg.sender, fl_o.ownerOf(fl_o.tokenByIndex(count)), amount / length);
             count++;
         }
     }
     
-    function dropERC721(address token) external { // drop NFT series on FL_O holders, starting from `tokenId` 1
-        IFL_O FL_O = IFL_O(FLAMING_O);
-        uint256 count = 1;
-        uint256 length = FL_O.totalSupply();
+    function dropERC721(address token) external { // drop parallel NFT series on FL_O holders
+        IERC721Listing fl_o = IERC721Listing(FLAMING_O);
+        uint256 count;
+        uint256 length = fl_o.totalSupply();
         
         for (uint256 i = 0; i < length; i++) {
-            IERC721transferFrom(token).transferFrom(msg.sender, FL_O.ownerOf(count), count);
+            IERC721Listing(token).transferFrom(msg.sender, fl_o.ownerOf(fl_o.tokenByIndex(count)), IERC721Listing(token).tokenByIndex(count));
             count++;
         }
     }
